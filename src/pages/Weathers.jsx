de65b78268c0ec3aba6577d649792b12ad223bb4
karimou cisse <karimou@pylote.io>
2023-07-11 14:06:10 +0200

    add change

--- src/pages/Weathers.jsx
+++ src/pages/Weathers.jsx
@@ -4,10 +4,13 @@ import SearchBar from '../components/SearchBar';
 import { Box, CircularProgress, Stack, Typography } from '@mui/material';
 import WeatherCard from '../components/WeatherCard';
 import SelectValue from '../components/SelectValue';
+import HouseIcon from '@mui/icons-material/House';
+import { useNavigate } from 'react-router-dom';
 const Weathers = () => {
   const [value, setValue] = useState();
   const [weatherData, setWeathersData] = useState([]);
   const [selectValue, setSelectValue] = useState('celsius');
+  const navigate = useNavigate();
   const getData = async () => {
     const data = await getWeatherDatas(value, selectValue);
     const dataArray = data ? [data] : [];
@@ -26,6 +29,12 @@ const Weathers = () => {
 
   return (
     <Box sx={{ padding: '40px 100px', backgroundColor: '#FAF9F8' }}>
+      <HouseIcon
+        onClick={() => navigate('/')}
+        color="#1c2930"
+        fontSize="large"
+        sx={{ cursor: 'pointer', marginBottom: '10px' }}
+      />
       <Stack direction="row" spacing={3}>
         <SearchBar setValue={setValue} placeHolder="Meteo" />
         <SelectValue
@@ -37,14 +46,14 @@ const Weathers = () => {
       <Box mt={6}>
         <Box
           sx={{
-            minHeight: '73vh',
+            minHeight: '63vh',
             display: 'flex',
             justifyContent: 'center',
             alignItems: 'center',
           }}
         >
           {!weatherData || weatherData.length === 0 ? (
-            <Typography>Il n'y a pas de données</Typography>
+            <Typography variant="h4">Il n'y a pas de données</Typography>
           ) : (
             weatherData.map((weather, i) => (
               <WeatherCard key={i} weather={weather} unit={selectValue} />
